e7c6b2bfcdadfe9a36081a56e4f395d09a3ec286
Nahida <51387274+NahidaMika@users.noreply.github.com>
2024-08-10 16:51:26 -0600

    Update MainScript.js

--- MainScript.js
+++ MainScript.js
@@ -87,25 +87,6 @@ myvid.addEventListener('ended', function(e) {
   myvid.play();
 });
 
-var myaudio = document.getElementById('Audiomusic');
-
-myaudio.addEventListener('ended', function(e) {
-  // get the active source and the next video source.
-  // I set it so if there's no next, it loops to the first one
-  var activesource = document.querySelector("#Audiomusic source.active");
-  var nextsource = document.querySelector("#Audiomusic source.active + source") || document.querySelector("#Audiomusic source:first-child");
-  
-  // deactivate current source, and activate next one
-  activesource.className = "";
-  nextsource.className = "active";
-  
-  // update the video source and play
-  myaudio.src = nextsource.src;
-  speed.selectedIndex =4;
-  myaudio.volume = volume.value;
-  myaudio.play();
-});
-
 var next = document.getElementById("next");
 
 next.addEventListener("click", function() {
