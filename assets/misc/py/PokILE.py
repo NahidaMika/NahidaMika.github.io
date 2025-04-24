3f816280d8a0a5bda6ce3c9bf148866928e2da4d
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-04-23 19:32:06 -0600

    add

---
+++ assets/misc/py/PokILE.py
@@ -0,0 +1,7 @@
+from random import uniform, randint
+
+#Satellite (Gross Michel) 1/6 to be destroyed
+
+#Supernova (Cavendish) 1/1000 to be destroyed
+
+#Ancient Joker Each played card with [suit] gives X1.5 Mult when scored,suit changes at end of round
--- assets/misc/py/ServicioSocial.py
+++ assets/misc/py/ServicioSocial.py
@@ -4,11 +4,64 @@ import pandas as pd
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl import load_workbook
+import json
+from tkinter import *
+from tkinter import filedialog
 
 import pandas as pd
 from datetime import datetime
 
 from io import BytesIO
+
+
+
+def getindividualdate():
+    completeDate = input("Enter the date in the format MM/DD/Hour In/Minute In/Hour Out/Minute Out/: ")
+    day, month, hour_in, minute_in, hour_out, minute_out = completeDate.split('/')
+    print(f"{month}/{day} {hour_in}:{minute_in}/{hour_out}:{minute_out}")
+    return month, day, hour_in, minute_in, hour_out, minute_out
+
+class ServicioSocial:
+    def __init__(self, month, day, hour_in, minute_in, hour_out, minute_out):
+        self.year = 2025
+        self.month = month
+        self.day = day
+        self.hour_in = hour_in
+        self.minute_in = minute_in
+        self.second_in = 0
+        self.hour_out = hour_out
+        self.minute_out = minute_out
+        self.second_out = 0
+
+    def save(self, path):
+            # map_file_path = 'SSIdiomas\\Json\\'
+            save_map = open(path, 'xt')# map_file_path + 
+            json.dump({'year': self.year,'month': self.month, 'day': self.day, 'hour_in': self.hour_in, 'minute_in': self.minute_in, 'second_in': self.second_in, 'hour_out': self.hour_out, 'minute_out': self.minute_out, 'second_out': self.second_out}, save_map)
+            save_map.close()
+
+    def load(self):
+        mapPath = filedialog.askopenfilename(initialdir="SSIdiomas\\Json",
+                                            title="Open file okay?",
+                                            filetypes= (("Map File","*.json"),
+                                            ("all files","*.*")))
+        map = open(mapPath,'r')
+        map_data = json.load(map)
+        map.close()
+
+        self.year = map_data['year']
+        self.month  = map_data['month']
+        self.day = map_data['day']
+        self.hour_in = map_data['hour_in']
+        self.minute_in = map_data['minute_in']
+        self.second_in = map_data['second_in']
+        self.hour_out = map_data['hour_out']
+        self.minute_out = map_data['minute_out']
+        self.second_out = map_data['second_out']
+
+    def showHours(self):
+        pass
+month, day, hour_in, minute_in, hour_out, minute_out = getindividualdate()
+ServicioSocial(month, day, hour_in, minute_in, hour_out, minute_out).save('ServicioSocial.json')
 
 def total_hours(hours):
     hours.total_seconds()/ 3600
@@ -106,15 +159,38 @@ dia_20_1 = datetime(2025,4,4, 10, 9, 0)
 dia_20_2 = datetime(2025,4,4, 18, 3, 0)
 dia_20 = dia_20_2 - dia_20_1
 
-total = dia_1 + dia_2 + dia_3 + dia_4 + dia_5 + dia_6 + dia_7 + dia_8 + (dia_9*2) + (dia_10*2) + (dia_11*2) + (dia_12 *2 ) + dia_13 + dia_14 + dia_15 + dia_16 + dia_17 + dia_18 + dia_19 + dia_20
+dia_21_1 = datetime(2025,4,7, 13, 18, 0)
+dia_21_2 = datetime(2025,4,7, 18, 7, 0)
+dia_21 = dia_21_2 - dia_21_1
+
+dia_22_1 = datetime(2025,4,9, 13, 18, 0)
+dia_22_2 = datetime(2025,4,9, 18, 9, 0)
+dia_22 = dia_22_2 - dia_22_1
+
+dia_23_1 = datetime(2025,4,10, 13, 50, 0)
+dia_23_2 = datetime(2025,4,10, 20, 0, 0) #+2 horas
+dia_23 = dia_23_2 - dia_23_1
+
+dia_24_1 = datetime(2025,4,12, 10, 4, 0)
+dia_24_2 = datetime(2025,4,12, 14, 30, 0)
+dia_24 = dia_24_2 - dia_24_1
+
+dia_25_1 = datetime(2025,4,9, 13, 0, 0)
+dia_25_2 = datetime(2025,4,9, 17, 30, 0)
+dia_25 = dia_25_2 - dia_25_1
+
+
+
+
+total = dia_1 + dia_2 + dia_3 + dia_4 + dia_5 + dia_6 + dia_7 + dia_8 + (dia_9*2) + (dia_10*2) + (dia_11*2) + (dia_12 *2 ) + dia_13 + dia_14 + dia_15 + dia_16 + dia_17 + dia_18 + dia_19 + dia_20 + dia_21 + dia_22 + (dia_23) + dia_24 
 
 H_180 = 10800
 quedante = H_180 - (total.total_seconds()/60)
 
-print(quedante)
+print(quedante) # lo que me queda en minutos
 
-print(quedante/60)
+print(quedante/60) # lo que me queda en horas
 
 print(f"{total.total_seconds()/60} / {H_180}")
 
-print(total_hours(total))
+print(total_hours(total)) #Total de horas
