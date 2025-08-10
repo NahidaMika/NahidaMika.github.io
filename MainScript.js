4ba886105c706a58b4d93487c48f45d7a0271635
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-08-10 01:37:47 -0600

    UPDATE

--- MainScript.js
+++ MainScript.js
@@ -42,33 +42,6 @@ volume.addEventListener("change", function() {
     myvid.volume = (volume.value/100);    
 });
 
-//var myvid = document.getElementById('MusicVideo');
-//var myvids = [
-    //"file:///C:\Users\tleyo\Desktop\html\music\【Animation MV】 What an amazing swing _角巻わため【original】.mp4", 
-    //"file:///C:\Users\tleyo\Desktop\html\music\【MV】 Oh! Asmara - Kobo Kanaeru.mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\【MV】HELP!! - Kobo Kanaeru.mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\【original anime MV】III【hololive_宝鐘マリン＆こぼ・かなえる】.mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\Overdose - Kobo Kanaeru (Cover).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『お勉強しといてよ』MV (ZUTOMAYO – STUDY ME).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『ハゼ馳せる果てるまで』MV.mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『嘘じゃない』MV (ZUTOMAYO – Truth In Lies).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『暗く黒く』MV（ZUTOMAYO – DARKEN）.mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『秒針を噛む』MV (ZUTOMAYO – Byoushinwo Kamu).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ずっと真夜中でいいのに。『綺羅キラー (feat. Mori Calliope)』MV (ZUTOMAYO – Kira Killer).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\ビビデバ _ 星街すいせい(official).mp4",
-    //"file:///C:\Users\tleyo\Desktop\html\music\美波「カワキヲアメク」MV.mp4"
-  //];
-//var activeVideo = 0;
-
-//myvid.addEventListener('ended', function(e) {
-  // update the new active video index
-  //activeVideo = (++activeVideo) % myvids.length;
-
-  // update the video source and play
-  //myvid.src = myvids[activeVideo];
-  //myvid.play();
-//});
-
 
 myvid.addEventListener('ended', function(e) {
   // get the active source and the next video source.
@@ -124,12 +97,6 @@ x
     }
 });
 
-//const gameredirect = document.getElementById("redirectbutton");
-
-//gameredirect.addEventListener("click", function() {
-    //window.location.href='http://192.168.0.3:8080/webserver2';
-//});
-
 const inputbox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
 
@@ -173,25 +140,6 @@ function loadData() {
 }
 
 loadData();
-
-// function json(url) {
-       // return fetch(url).then(res => res.json());
-      // }
-      
-      //let apiKey = 'your_api_key';
-      //json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-       // console.log(data.ip);
-       // console.log(data.city);
-       // console.log(data.country_code);
-        // so many more properties
-     // });    
-
-// function getIPFromAmazon() {
-    // fetch("https://checkip.amazonaws.com/").then(res => res.text()).then(data => console.log(data))
-// }
-      
-// Text to speech script
-//----------------------------------------------------------------
 
 let speech = new SpeechSynthesisUtterance();
 
